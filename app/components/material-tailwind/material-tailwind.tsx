46fa0932ff889e2c0260a9645f79db2f65863008
Andyveloper <mcantillo.gdi@gmail.com>
2023-08-07 01:29:36 -0500

    Create export file under use client config

---
+++ app/components/material-tailwind/material-tailwind.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import {
+  Navbar,
+  Collapse,
+  Typography,
+  Button,
+  IconButton,
+  List,
+  ListItem,
+  Menu,
+  MenuHandler,
+  MenuList,
+  MenuItem,
+  Chip,
+  ListItemPrefix,
+  ListItemSuffix,
+  Avatar,
+  Card,
+  CardBody
+} from "@material-tailwind/react";
+
+export {
+  Navbar,
+  Collapse,
+  Typography,
+  Button,
+  IconButton,
+  List,
+  ListItem,
+  Menu,
+  MenuHandler,
+  MenuList,
+  MenuItem,
+  Chip,
+  ListItemPrefix,
+  ListItemSuffix,
+  Avatar,
+  Card,
+  CardBody
+}
