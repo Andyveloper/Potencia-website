3bc99e456e49008ac319267dceb2ad010d17931a
Andyveloper <mcantillo.gdi@gmail.com>
2023-08-30 21:43:35 -0500

    Configure website next app

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ const nextConfig = {
     remotePatterns:[
       {
         hostname:'images.unsplash.com'
+      },
+      {
+        hostname: 'images.pexels.com'
       }
     ]
   }
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,11 @@ module.exports = withMT({
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      screens: {
+        '2xl': '1536px'
+      }
     },
   },
   plugins: [],
+
 })
